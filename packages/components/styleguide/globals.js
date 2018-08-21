da2f94ec8d88958f646d2fbf7ad09cb73c90fa42
mikethehud <mail@mikehudson.de>
2018-08-21 21:14:54 +1200

    Set up global loading and sections in styleguide config

---
+++ packages/components/styleguide/globals.js
@@ -0,0 +1,4 @@
+global.CardBody = require("../src/atoms/Card/CardBody").default;
+global.CardButtons = require("../src/atoms/Card/CardButtons").default;
+global.CardSubtitle = require("../src/atoms/Card/CardSubtitle").default;
+global.CardTitle = require("../src/atoms/Card/CardTitle").default;
