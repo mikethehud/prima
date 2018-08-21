c53f9013842d3751bd16db0f1ca83cf37376c8aa
mikethehud <mail@mikehudson.de>
2018-08-21 21:16:39 +1200

    Cards are a molecules not atoms

--- packages/components/styleguide/globals.js
+++ packages/components/styleguide/globals.js
@@ -1,4 +1,4 @@
-global.CardBody = require("../src/atoms/Card/CardBody").default;
-global.CardButtons = require("../src/atoms/Card/CardButtons").default;
-global.CardSubtitle = require("../src/atoms/Card/CardSubtitle").default;
-global.CardTitle = require("../src/atoms/Card/CardTitle").default;
+global.CardBody = require("../src/molecules/Card/CardBody").default;
+global.CardButtons = require("../src/molecules/Card/CardButtons").default;
+global.CardSubtitle = require("../src/molecules/Card/CardSubtitle").default;
+global.CardTitle = require("../src/molecules/Card/CardTitle").default;
