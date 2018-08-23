e9b4e2e8dc17de7043ee62449a3c677f3c9202cb
mikethehud <mail@mikehudson.de>
2018-08-23 22:50:05 +1200

    Basic ButtonGroup tests

---
+++ packages/components/src/__tests__/ButtonGroup.spec.tsx
@@ -0,0 +1,34 @@
+/* tslint:disable:jsx-no-lambda */
+
+import { mount, shallow } from "enzyme"
+import * as React from "react"
+import Button from "../atoms/Button/Button"
+import ButtonGroup from "../molecules/ButtonGroup/ButtonGroup"
+
+describe("ButtonGroup", () => {
+  it("renders correctly", () => {
+    const wrapper = shallow(<ButtonGroup />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it("renders with one button inside", () => {
+    const wrapper = mount(
+      <ButtonGroup>
+        <Button text="Hello" />
+      </ButtonGroup>
+    )
+    expect(wrapper.find(".button").length).toEqual(1)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it("renders with multiple buttons inside", () => {
+    const wrapper = mount(
+      <ButtonGroup>
+        <Button text="Hello" />
+        <Button text="Hello" />
+      </ButtonGroup>
+    )
+    expect(wrapper.find(".button").length).toEqual(2)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
